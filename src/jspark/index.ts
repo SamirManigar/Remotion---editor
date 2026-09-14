c043ebfc7c70fff65a0e189efc23a26c4d5f0e2f
SAMIR MANIGAR <sameermanigar643@gmail.com>
2026-09-14 22:02:39 +0530

    Fix Remotion entry TypeScript syntax

--- src/jspark/index.ts
+++ src/jspark/index.ts
@@ -2,15 +2,14 @@ import React from "react";
 import {Composition, registerRoot} from "remotion";
 import {JsparkShort, JSPARK_DURATION, JSPARK_FPS} from "./JsparkShort";
 
-const JsparkRoot: React.FC = () => (
-	<Composition
-		id="JsparkShort"
-		component={JsparkShort}
-		durationInFrames={JSPARK_DURATION}
-		fps={JSPARK_FPS}
-		width={1080}
-		height={1920}
-	/>
-);
+const JsparkRoot: React.FC = () =>
+	React.createElement(Composition, {
+		id: "JsparkShort",
+		component: JsparkShort,
+		durationInFrames: JSPARK_DURATION,
+		fps: JSPARK_FPS,
+		width: 1080,
+		height: 1920,
+	});
 
 registerRoot(JsparkRoot);
