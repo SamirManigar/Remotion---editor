9ae97121b80da8fbf587e2889d00f3d5320ba861
SAMIR MANIGAR <sameermanigar643@gmail.com>
2026-09-14 21:57:58 +0530

    Register Jspark short composition

---
+++ src/jspark/index.ts
@@ -0,0 +1,16 @@
+import React from "react";
+import {Composition, registerRoot} from "remotion";
+import {JsparkShort, JSPARK_DURATION, JSPARK_FPS} from "./JsparkShort";
+
+const JsparkRoot: React.FC = () => (
+	<Composition
+		id="JsparkShort"
+		component={JsparkShort}
+		durationInFrames={JSPARK_DURATION}
+		fps={JSPARK_FPS}
+		width={1080}
+		height={1920}
+	/>
+);
+
+registerRoot(JsparkRoot);
